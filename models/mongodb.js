e965d7079ecd32ea21ca86612f5dc03a7b921c78
luis-moguea <luis.mogueadp@gmail.com>
2023-03-26 18:42:13 -0500

    setting up swagger

--- models/mongodb.js
+++ models/mongodb.js
@@ -3,7 +3,7 @@ const path = require("path")
 const dotenv = require("dotenv").config({path: path.resolve(__dirname, "../.env")})
 const mongooseHidden = require("mongoose-hidden")()
 
-const mongoUri = `mongodb+srv://luismogueadp:${process.env.PASSWORD}@cluster0.n9lxkrd.mongodb.net/todo-list?retryWrites=true&w=majority`
+const mongoUri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.n9lxkrd.mongodb.net/todo-list?retryWrites=true&w=majority`
 
 mongoose.connect(mongoUri).then(()=>{
     console.log("Database connected")
