618b3534ad95fd1771a92c236db18d6c443c3616
Andres Campuzano Garzon <andres@Andress-MacBook-Air.local>
2023-03-31 17:54:40 -0500

    update readme, adds jsDocs

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,18 +1,15 @@
 const swaggerJsDoc = require("swagger-jsdoc")
 const swaggerUi = require("swagger-ui-express")
-const path = require("path")
-const { join } = require("path")
-const { application } = require("express")
 
-
-//Metadata info about the api
-
+/**
+ * Creates the swagger options - JsDocs
+ */
 const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-        title: "To-Do List",
-        version: "1.0.0"
+            title: "To-Do List",
+            version: "1.0.0"
         },
 
         servers: [
@@ -29,11 +26,11 @@ const options = {
 
 const swaggerSpec = swaggerJsDoc(options)
 
-// funtion to setup our docs
+// function to setup our docs
 
 const swaggerDocs = (app, port) => {
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-    console.log(`Version 1 docs are available at https://express-todo-list-e9ic.onrender.com/api/docs`)
+    console.log(`Version 1 docs are available at https://express-todo-list-noe9.onrender.com/api-docs/`)
 }
 
 module.exports = { swaggerDocs }
